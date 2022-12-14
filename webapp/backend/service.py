abf6d527fcc424953c36556c515d8fa12360d812
Peter Wu <peterlingwu@gmail.com>
2022-12-14 02:45:24 -0500

    Fully implement Flask service and web UI

--- webapp/backend/service.py
+++ webapp/backend/service.py
@@ -1,7 +1,143 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import pickle as pkl
+import pandas as pd
+import numpy as np
+import os
+import json
+from datetime import datetime
+import requests
+import re
+import nltk
+import textblob 
+from textblob import TextBlob
+import pickle as pkl
+from IPython.display import display
+from IPython.core.magic import register_line_cell_magic
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('punkt')
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+import lightgbm 
+from lightgbm import LGBMRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import xgboost
 
 app = Flask(__name__)
 
-@app.route('/api/inference')
-def hello():
-    return 'Hello, World!'
+# Getting stock price changes (by percent of change), keys are stock tickers
+lgbm_model = pkl.load(open('lgbm.pkl', 'rb'))
+lr_model = pkl.load(open('LR.pkl', 'rb'))
+rf_model = pkl.load(open('RF.pkl', 'rb'))
+xgb_model = pkl.load(open('xgb.pkl', 'rb'))
+
+'''
+STATIC HELPERS
+'''
+# Lemmatizing the text
+def lemmatize(text):
+    words = nltk.word_tokenize(text)
+    lem_words = [WordNetLemmatizer().lemmatize(s.lower()) for s in words]
+    result = ' '.join(lem_words)
+    return result
+
+# Remove everything but the word stems for each word in the text
+def stem(text):
+    ps = PorterStemmer()
+    words = nltk.word_tokenize(text)
+    stemmed_words = [ps.stem(s) for s in words]
+    result = ' '.join(stemmed_words)
+    return result
+
+# Remove Punctuation from the text
+def removePunc(text):
+    return re.sub(r'[^\w\s]', '', text)
+
+def removeBreaks(text):
+    text = text.replace('\\n', '')
+    text = text.replace('\\t', '')
+    text = text.replace('\\r', '')
+    return text
+
+def compute_finbert_probabilities(text):
+  """
+  Uses FinBERT via REST to compute the sentiment analysis logits
+  given a string of text
+
+  Return format:
+  {"POSITIVE": number, "NEGATIVE": number, "NEUTRAL": number}
+  """
+  url = "https://finbert3.p.rapidapi.com/sentiment/en"
+
+  payload = {"text": text}
+  headers = {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": os.environ.get('RAPID_API_KEY'),
+    "X-RapidAPI-Host": "finbert3.p.rapidapi.com"
+  }
+
+  response = requests.request("POST", url, json=payload, headers=headers)
+
+  res_dict = json.loads(response.text)
+  return res_dict["sentiment_probabilities"]
+
+
+# need to tokenize into sentences, feed that into textblob
+# get sentences with < 2000 characters
+def most_subjective(text):
+  sentences = nltk.sent_tokenize(text)
+  scores = []
+  for sentence in sentences:
+    subjectivity = TextBlob(sentence).sentiment.subjectivity
+    scores.append((sentence, subjectivity))
+  scores.sort(key = lambda x : x[1], reverse = True)
+  total_char = 0
+  text_sents = []
+  for tup in scores:
+    total_char += len(tup[0])
+    if total_char < 2000:
+      text_sents.append(tup[0])
+    elif len(text_sents) > 0:
+      break
+  result = ' '.join(text_sents)
+  return result
+
+@app.route('/api/inference', methods=["POST"])
+def inference():
+    req = request.get_json(force=True)
+    text = req.get('text', None) 
+    model_type = req.get('modelType', None)
+
+    nlp_text = most_subjective(text)
+    nlp_text = lemmatize(text)
+    nlp_text = stem(text)
+    nlp_text = removePunc(text)
+    sentiment_scores = compute_finbert_probabilities(nlp_text)
+    positivity = sentiment_scores['POSITIVE']
+    neutral = sentiment_scores['NEUTRAL']
+    negativity = sentiment_scores['NEGATIVE']
+    data = dict(positivity=positivity, neutral=neutral, negativity=negativity)
+    df = pd.DataFrame(data, index=[0])
+    print('NLP Analysis of 10-K Text')
+    print(df)
+
+    model = None
+    print('Model: ' + model_type)
+    if (model_type == 'Linear Regression'):
+        model = lr_model
+    elif (model_type == 'Random Forest'):
+        model = rf_model
+    elif (model_type == 'XGBoost'):
+        model = xgb_model
+    elif (model_type == 'Light Gradient Boosting Machine'):
+        model = lgbm_model
+
+    preds = model.predict(df).tolist()
+    print(preds)
+    return jsonify(data=preds)
--- webapp/frontend/src/App.js
+++ webapp/frontend/src/App.js
@@ -18,27 +18,27 @@ import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  PointElement,
-  LineElement,
+  BarElement,
   Title,
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import { useState, useEffect } from 'react';
 import CompanyTickers from "./company_tickers.json";
-
+import CompanyPrices from "./company_tickers_prices.json";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  PointElement,
-  LineElement,
+  BarElement,
   Title,
   Tooltip,
   Legend
 );
-
-const labels = ['2016', '2017', '2018', '2019', '2020'];
 
 export const options = {
   responsive: true,
@@ -48,84 +48,71 @@ export const options = {
     },
     title: {
       display: true,
-      text: 'Comparing Results from Inference with Actual Data',
+      text: 'Comparing Percents for Predicted vs Real Stock Price Change',
     },
   },
+  // maintainAspectRatio: 
 };
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Actual data',
-      data: labels.map(() => Math.random()),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Predictions',
-      data: labels.map(() => Math.random()),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
+const labels = ['Dataset'];
 
-const tickers = Array(11643).fill().map((curr, index) => CompanyTickers[index].ticker);
-
-function ComboBox() {
-  return (
-    <Autocomplete
-      disablePortal
-      id="combo-box-demo"
-      options={tickers}
-      sx={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label="Ticker" />}
-    />
-  );
-}
-
-const card1 = (
-  <>
-    <CardContent>
-      <Typography variant="h5" component="div">
-        Inference
-      </Typography>
-      <br />
-      <ComboBox />
-      <br />
-      <TextField
-        placeholder="Enter 10-K filing M&A (Item 7) text here."
-        multiline
-        style={{ width: "100%" }}
-        rows={15}
-      />
-    </CardContent>
-    <CardActions>
-      <Button variant="outlined" onClick={() => alert("yay")}>Perform Inference</Button>
-    </CardActions>
-  </>
-);
-
-
-const card2 = (
-  <>
-    <CardContent>
-      <Typography variant="h5" component="div">
-        Results
-      </Typography>
-      <br />
-      <Line options={options} data={data} />
-      <CardActions>
-      </CardActions>
-    </CardContent>
-  </>
-);
-
+const tickers = Array(11643).fill().map((_, index) => CompanyTickers[index].ticker);
 
 function App() {
   const [isGraphRendered, setIsGraphRendered] = useState(false);
   const [isLoadingData, setIsLoadingData] = useState(false);
+  const [text, setText] = useState(null);
+  const [ticker, setTicker] = useState(null);
+  const [modelType, setModelType] = useState("Linear Regression");
+  const [year, setYear] = useState("2021");
+  const [chartData, setChartData] = useState(null);
+  const handleChange = (event) => {
+    setModelType(event.target.value);
+  };
+
+  const findTickerPriceChange = (ticker, year) => {
+    for (let i = 0; i < 11643; i++) {
+      const data = CompanyPrices[i];
+      if (data.ticker === ticker) {
+        return data.stock[year];
+      }
+    }
+    throw new Error("unable to find ticker!");
+  }
+
+  const handleInference = async () => {
+    setIsGraphRendered(false);
+    setIsLoadingData(true);
+    console.log(modelType);
+    console.log(text);
+    console.log(ticker);
+    const res = await fetch('/api/inference', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ text, modelType })
+    })
+    const val = await res.json();
+    setChartData({
+      labels,
+      datasets: [
+        {
+          label: 'Predicted',
+          data: [Number(val.data[0]) * 100],
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+          label: 'Actual',
+          data: [Number(findTickerPriceChange(ticker, year)) * 100],
+          backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+      ],
+    });
+    setIsGraphRendered(true);
+    setIsLoadingData(false);
+  }
+
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
@@ -144,14 +131,111 @@ function App() {
       <Container maxWidth="xl">
         <Grid container spacing={2}>
           <Grid item xs={6}>
-            <Card variant="outlined" style={{ minHeight: "75vh" }}>{card1}</Card>
+            <Card variant="outlined" style={{ minHeight: "75vh" }}><>
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  Inference
+                </Typography>
+                <br />
+                <Autocomplete
+                  disablePortal
+                  id="combo-box-demo"
+                  options={tickers}
+                  onChange={(_, values) => setTicker(values)}
+                  sx={{ width: 300 }}
+                  renderInput={(params) => <TextField {...params} label="Ticker" />}
+                />
+                <br />
+                <TextField
+                  placeholder="Enter 10-K filing M&A (Item 7) text here."
+                  multiline
+                  onChange={(event) => setText(event.target.value)}
+                  style={{ width: "100%" }}
+                  rows={9}
+                />
+                <br />
+                <InputLabel id="demo-simple-select-label">Model to use</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={modelType}
+                  label="Model Type"
+                  defaultValue={"Linear Regression"}
+                  onChange={handleChange}
+                >
+                  <MenuItem value={"Linear Regression"}>Linear Regression</MenuItem>
+                  <MenuItem value={"Random Forest"}>Random Forest</MenuItem>
+                  <MenuItem value={"XGBoost"}>XGBoost</MenuItem>
+                  <MenuItem value={"Light Gradient Boosting Machine"}>Light Gradient Boosting Machine</MenuItem>
+                </Select>
+                <br />
+                <InputLabel id="year-label">Year of filing</InputLabel>
+                <Select
+                  labelId="year-label"
+                  id="year-simple-select"
+                  value={year}
+                  label="Year of filing"
+                  defaultValue={"2017"}
+                  onChange={(event) => setYear(event.target.value)}
+                >
+                  <MenuItem value={"2017"}>2017</MenuItem>
+                  <MenuItem value={"2018"}>2018</MenuItem>
+                  <MenuItem value={"2019"}>2019</MenuItem>
+                  <MenuItem value={"2020"}>2020</MenuItem>
+                  <MenuItem value={"2021"}>2021</MenuItem>
+                </Select>
+              </CardContent>
+              <CardActions>
+                <Button variant="outlined" onClick={() => handleInference()}>Perform Inference</Button>
+              </CardActions>
+            </></Card>
           </Grid>
           <Grid item xs={6}>
-            <Card variant="outlined" style={{ minHeight: "75vh" }}>{
-              isLoadingData ?
-                <CircularProgress />
-                : card2
-            }</Card>
+            <Card variant="outlined" style={{
+              minHeight: "75vh",
+              display: "flex",
+              alignItems: (isGraphRendered || isLoadingData) ? "center" : undefined,
+              justifyContent: (isGraphRendered || isLoadingData) ? "center" : undefined
+            }}>{
+                isLoadingData ?
+                  <CircularProgress size={60} className="progress" />
+                  : <>
+                    <CardContent>
+                      <Typography variant="h5" component="div">
+                        Results
+                      </Typography>
+                      <br />
+                      {isGraphRendered ? <Bar options={{
+                        responsive: true,
+                        plugins: {
+                          legend: {
+                            position: 'top',
+                          },
+                          title: {
+                            display: true,
+                            text: 'Comparing Percents for Predicted vs Real Stock Price Change in '
+                              + year + ' for ' + ticker,
+                          },
+                        },
+                      }}
+                        data={chartData} width={"500px"} height={"500px"} /> :
+                        <>
+                          <Typography variant="h6" color="grey">
+                            No inference has been conducted yet!
+                            Please enter your data and click 'Perform Inference' to begin.
+                          </Typography>
+                          <br />
+                          <Typography color="grey">
+                            How it works: this will set off the same pipeline as in the
+                            Jupyter Notebook, where we conduct NLP methods to do feature engineering,
+                            and then do predictions with your selected model.
+                          </Typography>
+                        </>}
+                      <CardActions>
+                      </CardActions>
+                    </CardContent>
+                  </>
+              }</Card>
           </Grid>
         </Grid>
       </Container>
